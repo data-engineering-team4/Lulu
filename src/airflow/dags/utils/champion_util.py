c211192ed0498ab2c080a0ba74cd204c9886c467
qkr1839 <qkr1839@gmail.com>
2023-08-12 19:02:16 +0900

    chore: 파일 최신화 및 경로 수정

--- src/airflow/dags/utils/champion_util.py
+++ src/airflow/dags/utils/champion_util.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def champion_info_extraction():
     """
@@ -6,11 +7,16 @@ def champion_info_extraction():
     2. champion_info_extraction 실행하면 champion_dictionary.json이 업데이트 됨
     3. 신챔프 추가시 champion_mapping_ko_en.json을 수작업으로 추가 해줘야 한다.
     """
-    with open("champion.json", "r") as file:
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    champion_path = os.path.join(current_directory, "champion.json")
+    champion_dict_path = os.path.join(current_directory, "champion_dictionary.json")
+
+    with open(champion_path, "r", encoding="utf-8") as file:
         json_data = file.read()
 
     data = json.loads(json_data)
     champion_dict = {champion["key"]: champion["name"] for champion in data["data"].values()}
+    print(len(champion_dict))
 
-    with open("champion_dictionary.json", "w") as file:
+    with open(champion_dict_path, "w") as file:
         json.dump(champion_dict, file)
