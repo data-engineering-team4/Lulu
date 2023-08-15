32eaaf8514a548ffb33e7f223d03e1e33db98902
fluencygod <fluencygod@gmail.com>
2023-08-16 02:30:42 +0900

    Feat connect dags with kafka

--- src/airflow/dags/get_high_elo_summoner_dag.py
+++ src/airflow/dags/get_high_elo_summoner_dag.py
@@ -1,11 +1,41 @@
-import json
-
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
+def get_high_elo_summoners_by_tier():
+
+    from utils.riot_util import get_high_elo_summoner_info
+    import time
+    import logging
+    from airflow.models import Variable
+    import json
+    from kafka import KafkaProducer
+
+    logging.basicConfig(level=logging.INFO)
+
+    producer = KafkaProducer(bootstrap_servers='kafka:9092')
+
+    # TODO API_KEY Airflow에서 가져옴 -> AWS Secrets Manager로 대체하기
+    api_key = Variable.get("RIOT_KEY_1")
+
+    high_elo_list = ["challenger", "grandmaster", "master"]
+
+    for high_elo in high_elo_list:
+        json_data = get_high_elo_summoner_info(high_elo, api_key)
+        # logging.info(json_data)
+        for data in json_data['entries']:
+            summoner_data = {
+                'high_elo': high_elo,
+                'summoner_id': data['summonerId'],
+                'summoner_name': data['summonerName'],
+            }
+            value = json.dumps(summoner_data).encode('utf-8')
+            producer.send('summoner_data', value=value)
+        time.sleep(1.2)
+    producer.send('summoner_data', value=json.dumps('finish').encode('utf-8'))
+    producer.flush()
 
 with DAG(
     dag_id = 'get_high_elo_summoners_by_tier',
@@ -17,46 +47,10 @@ with DAG(
 
     start = EmptyOperator(task_id='start')
 
-    def get_high_elo_summoners_by_tier(**context):
-
-        from utils.riot_util import get_high_elo_summoner_info
-        import time
-        import logging
-        from airflow.models import Variable
-
-        logging.basicConfig(level=logging.INFO)
-
-        # TODO API_KEY Airflow에서 가져옴 -> AWS Secrets Manager로 대체하기
-        api_key = Variable.get("RIOT_KEY_1")
-
-        high_elo_list = ["challenger", "grandmaster", "master"]
-        summoner_data_list = []
-
-        for high_elo in high_elo_list:
-            json_data = get_high_elo_summoner_info(high_elo, api_key)
-
-            for data in json_data['entries']:
-                summoner_data_list.append({
-                    'high_elo': high_elo,
-                    'summoner_id': data['summonerId'],
-                    'summoner_name': data['summonerName'],
-                })
-
-            time.sleep(1.2)
-
-        return summoner_data_list
-
-    def push_summoner_data_to_xcom(**context):
-        summoner_data_list = get_high_elo_summoners_by_tier(**context)
-        context['task_instance'].xcom_push(key='summoner_data_list', value=json.dumps(summoner_data_list))
-        return summoner_data_list
-
-
-    push_summoner_data_to_xcom_op = PythonOperator(
-        task_id='push_summoner_data_to_xcom',
-        python_callable=push_summoner_data_to_xcom,
+    get_high_elo_summoners_by_tier = PythonOperator(
+        task_id='get_high_elo_summoners_by_tier',
+        python_callable=get_high_elo_summoners_by_tier,
         provide_context=True,
-        dag=dag
     )
 
     trigger_get_puuid_dag = TriggerDagRunOperator(
@@ -66,4 +60,4 @@ with DAG(
 
     end = EmptyOperator(task_id='end')
 
-    start >> push_summoner_data_to_xcom_op >> trigger_get_puuid_dag >> end
+    start >> get_high_elo_summoners_by_tier >> trigger_get_puuid_dag >> end
