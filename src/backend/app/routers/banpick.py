434033ef291be7ab841ec1d210b2bd2f52efbdc5
fluencygod <fluencygod@gmail.com>
2023-08-28 14:06:16 +0900

    Feat kafka producer

--- src/backend/app/routers/banpick.py
+++ src/backend/app/routers/banpick.py
@@ -1,14 +1,25 @@
 from fastapi import APIRouter
 from ..models.team_info import TeamInfo
 from ..models.summoner_info import SummonerInfo
+import os
+from confluent_kafka import Producer
 
 router = APIRouter()
+
+# todo fastapi는 비동기 프레임워크, ThreadPoolExecutor 등을 사용하여 프로듀서와 컨슈머를 비동기로 처리하면
+# I/O 바운드 작업이나 HTTP 요청 처리하는 등 다른 비동기 작업을 동시에 수행하여 다수의 클라이언트와 동시에 통신할 때 효율적
+
+develop_server_address = ''
+bootstrap_servers = os.environ.get('KAFKA_BOOTSTRAP_SERVERS', develop_server_address)
+p = Producer({'bootstrap.servers': bootstrap_servers})
 
 
 @router.post("/banpick/produce")
 async def get_team_info(team_info: TeamInfo):
     print("Received data:", team_info)
-    # 카프카로 보내는 로직
+    p.produce('testfastapi', value=str(team_info.dict()))
+    p.flush()
+
     return {"ourTeam": team_info.ourTeam, "opponentTeam": team_info.opponentTeam}
 
 
---
+++ src/backend/app/utils/consumer_test.py
@@ -0,0 +1,32 @@
+from confluent_kafka import Consumer, KafkaError
+
+# 카프카 컨슈머 설정
+config = {
+    'bootstrap.servers': '',
+    'group.id': 'your_group_id',
+    'auto.offset.reset': 'earliest'
+}
+consumer = Consumer(config)
+
+# 구독할 토픽 설정
+consumer.subscribe(['testfastapi'])
+
+try:
+    while True:
+        msg = consumer.poll(1.0)
+
+        if msg is None:
+            continue
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                continue
+            else:
+                print(msg.error())
+                break
+
+        print(f"Received message: {msg.value().decode('utf-8')}")
+
+except KeyboardInterrupt:
+    pass
+finally:
+    consumer.close()
