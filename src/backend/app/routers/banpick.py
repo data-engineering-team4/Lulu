fce200e4ec997a677bdbbd74cf098aebd23f4276
fluencygod <fluencygod@gmail.com>
2023-08-31 03:48:49 +0900

    Update json_data for analysis

--- src/backend/app/routers/banpick.py
+++ src/backend/app/routers/banpick.py
@@ -23,6 +23,28 @@ client = boto3.client(
     region_name="ap-northeast-3",
 )
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(dir_path, 'champions.json'), 'r') as f:
+    champion_data = json.load(f)
+
+champion_mapping = {}
+for k, v in champion_data.items():
+    champion_mapping[int(k)] = v
+
+lane_mapping = {
+    0: "ALL",
+    1: "TOP",
+    2: "JUNGLE",
+    3: "MIDDLE",
+    4: "BOTTOM",
+    5: "UTILITY",
+    6: "TOP",
+    7: "JUNGLE",
+    8: "MIDDLE",
+    9: "BOTTOM",
+    10: "UTILITY"
+}
+
 
 def get_db():
     db = SessionLocal()
@@ -35,9 +57,32 @@ def get_db():
 @router.post("/banpick/produce")
 async def get_team_info(team_info: TeamInfo):
     print("Received data:", team_info)
+    my_lane = lane_mapping.get(team_info.myLane+1)
+    our_team = {}
+    opponent_team = {}
+
+    for lane, champ_id in team_info.ourTeam.items():
+        new_lane = lane_mapping.get(int(lane))
+        new_champ = champion_mapping.get(champ_id+1)
+        our_team[new_lane] = new_champ
+
+    for lane, champ_id in team_info.opponentTeam.items():
+        new_lane = lane_mapping.get(int(lane))
+        new_champ = champion_mapping.get(champ_id+1)
+        opponent_team[new_lane] = new_champ
+
+    transformed_data = {
+        'myLane': my_lane,
+        'ourTeam': our_team,
+        'opponentTeam': opponent_team
+    }
+    json_data = json.dumps(transformed_data)
+
     try:
         response = client.put_record(
-            StreamName=kinesis_stream_name, Data=team_info.to_json(), PartitionKey="partition_key"
+            StreamName=kinesis_stream_name,
+            Data=json_data,
+            PartitionKey="partition_key",
         )
     except Exception as e:
         print("Kinesis Error", e)
