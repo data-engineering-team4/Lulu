d3428c0ef5f4ae1d83e7f2096ccc2b4e0fb08f98
fluencygod <fluencygod@gmail.com>
2023-08-31 02:06:43 +0900

    Fix black test

--- src/airflow/dags/combine_match_dag.py
+++ src/airflow/dags/combine_match_dag.py
@@ -24,7 +24,7 @@ with DAG(
         aws_secret_access_key = Variable.get("aws_secret_access_key")
         bucket_name = Variable.get("bucket_name")
 
-        current_date = datetime.now().strftime('%Y-%m-%d')
+        current_date = datetime.now().strftime("%Y-%m-%d")
 
         s3_client = boto3.client(
             "s3",
@@ -32,22 +32,26 @@ with DAG(
             aws_secret_access_key=aws_secret_access_key,
         )
 
-        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix='data/match/')
-        all_files = [obj['Key'] for obj in response['Contents'] if obj['Key'].endswith('.csv')]
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix="data/match/")
+        all_files = [
+            obj["Key"] for obj in response["Contents"] if obj["Key"].endswith(".csv")
+        ]
         final_df = pd.DataFrame()
 
         for file in all_files:
             logging.info(file)
             csv_obj = s3_client.get_object(Bucket=bucket_name, Key=file)
-            body = csv_obj['Body'].read().decode('utf-8')
+            body = csv_obj["Body"].read().decode("utf-8")
             temp_df = pd.read_csv(StringIO(body))
             final_df = pd.concat([final_df, temp_df])
 
         final_csv_buffer = StringIO()
         final_df.to_csv(final_csv_buffer, index=False)
-        s3_client.put_object(Bucket=bucket_name, Body=final_csv_buffer.getvalue(),
-                             Key=f"data/adhoc_match/{current_date}/merged.csv")
-
+        s3_client.put_object(
+            Bucket=bucket_name,
+            Body=final_csv_buffer.getvalue(),
+            Key=f"data/adhoc_match/{current_date}/merged.csv",
+        )
 
     start = EmptyOperator(task_id="start")
 
