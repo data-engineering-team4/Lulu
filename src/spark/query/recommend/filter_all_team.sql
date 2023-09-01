c1da2465c1c5e0f5639bded08de741adaf4b1de0
fluencygod <fluencygod@gmail.com>
2023-09-01 23:10:03 +0900

    Fix filter_all_team.sql

--- src/spark/query/recommend/filter_all_team.sql
+++ src/spark/query/recommend/filter_all_team.sql
@@ -1,4 +1,4 @@
-SELECT f.*
-FROM our_filtered_data f
-JOIN opponent_filtered_data o
-ON f.match_id = o.match_id AND f.team_id = o.team_id;
+    SELECT Distinct f.*
+    FROM our_filtered_data f
+    JOIN opponent_filtered_data o
+    ON f.match_id = o.match_id AND f.team_id = o.team_id;
