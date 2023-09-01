c210041087bb20cda658b14d4646610b32f2eb43
fluencygod <fluencygod@gmail.com>
2023-09-02 03:23:17 +0900

    Update query considering the pick and win rates

--- src/spark/query/recommend/recommend_our_team.sql
+++ src/spark/query/recommend/recommend_our_team.sql
@@ -1,7 +1,19 @@
-SELECT champion_name,
-       AVG(win) * 100 AS `Win Rate`,
-       COUNT(match_id) AS `Total Games`
-FROM our_filtered_data
-WHERE position = "{my_lane}"
-GROUP BY champion_name
-ORDER BY `Win Rate` DESC
+(SELECT champion_name,
+        AVG(win) * 100 AS `Win Rate`,
+        COUNT(match_id) AS `Total Games`
+ FROM our_filtered_data
+ WHERE position = "{my_lane}"
+ GROUP BY champion_name
+ ORDER BY `Total Games` DESC
+ LIMIT 2)
+ 
+UNION ALL
+
+(SELECT champion_name,
+        AVG(win) * 100 AS `Win Rate`,
+        COUNT(match_id) AS `Total Games`
+ FROM our_filtered_data
+ WHERE position = "{my_lane}"
+ GROUP BY champion_name
+ ORDER BY `Win Rate` DESC
+ LIMIT 1)
