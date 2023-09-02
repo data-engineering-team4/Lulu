52b46d39d02efb94db7fbc477b8fcb91caa3b341
fluencygod <fluencygod@gmail.com>
2023-09-02 16:04:31 +0900

    Update the way data is imported from pick2 win1 to pick3

--- src/spark/query/recommend/recommend_opponent_team.sql
+++ src/spark/query/recommend/recommend_opponent_team.sql
@@ -1,19 +1,8 @@
-(SELECT champion_name,
+SELECT champion_name,
         AVG(win) * 100 AS `win_rate`,
         COUNT(match_id) AS `pick_rate`
  FROM opponent_filtered_data
  WHERE position = "{my_lane}"
  GROUP BY champion_name
  ORDER BY `pick_rate` DESC
- LIMIT 2)
- 
-UNION ALL
-
-(SELECT champion_name,
-        AVG(win) * 100 AS `win_rate`,
-        COUNT(match_id) AS `pick_rate`
- FROM opponent_filtered_data
- WHERE position = "{my_lane}"
- GROUP BY champion_name
- ORDER BY `win_rate` DESC
- LIMIT 1)
+ LIMIT 3
